7867ff92fde665e3b946dd1274309b7fad984bab
ainz <ainz713@gmail.com>
2021-05-05 11:47:03 +0300

    1. Quartz [#493878]

---
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -0,0 +1,52 @@
+package ru.job4j.quartz;
+
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import static org.quartz.JobBuilder.*;
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
+
+public class AlertRabbit {
+    public static void main(String[] args) {
+        try {
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            JobDetail job = newJob(Rabbit.class).build();
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(AlertRabbit.load())
+                    .repeatForever();
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException | IOException se) {
+            se.printStackTrace();
+        }
+    }
+
+    public static class Rabbit implements Job {
+        @Override
+        public void execute(JobExecutionContext context) throws JobExecutionException {
+            System.out.println("Rabbit runs here ...");
+        }
+    }
+
+    public static int load() throws IOException {
+        int c = 0;
+        try (BufferedReader rd = new BufferedReader(new FileReader("rabbit.properties"))) {
+            for (String line = rd.readLine(); line != null; line = rd.readLine()) {
+                if (line.contains("=")) {
+                    String[] line1 = line.split("=");
+                    c = Integer.parseInt(line1[1]);
+                }
+            }
+        }
+        return c;
+    }
+}
