ec1f60466abe494c4dc9e4fcf3c63f8fcfffb3ab
ainz <ainz713@gmail.com>
2021-05-07 12:45:41 +0300

    2.1. Преобразование даты [#493888] исправление

--- src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
@@ -3,11 +3,29 @@ package ru.job4j.grabber.utils;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SqlRuDateTimeParser implements DateTimeParser {
+    Map<Long, String> monthName = new HashMap<>();
 
     @Override
     public LocalDateTime parse(String parse56) {
+        monthName.put(1L, "янв");
+        monthName.put(2L, "фев");
+        monthName.put(3L, "мар");
+        monthName.put(4L, "апр");
+        monthName.put(5L, "май");
+        monthName.put(6L, "июн");
+        monthName.put(7L, "июл");
+        monthName.put(8L, "авг");
+        monthName.put(9L, "сен");
+        monthName.put(10L, "окт");
+        monthName.put(11L, "ноя");
+        monthName.put(12L, "дек");
+
         String[] s = parse56.replaceAll("[,]", "")
         .replaceAll("[:]", " ").split(" ");
 
@@ -15,35 +33,15 @@ public class SqlRuDateTimeParser implements DateTimeParser {
             return LocalDate.now().atTime(Integer.parseInt(s[1]), Integer.parseInt(s[2]));
         } else if (s[0].contains("вчера")) {
             return LocalDate.now().atTime(Integer.parseInt(s[1]), Integer.parseInt(s[2])).minusDays(1);
-        } else if (s[1].contains("дек")) {
-            s[1] = "12";
-        } else if (s[1].contains("ноя")) {
-            s[1] = "11";
-        } else if (s[1].contains("окт")) {
-            s[1] = "10";
-        } else if (s[1].contains("сент")) {
-            s[1] = "09";
-        } else if (s[1].contains("авг")) {
-            s[1] = "08";
-        } else if (s[1].contains("июл")) {
-            s[1] = "07";
-        } else if (s[1].contains("июн")) {
-            s[1] = "06";
-        } else if (s[1].contains("май")) {
-            s[1] = "05";
-        } else if (s[1].contains("апр")) {
-            s[1] = "04";
-        } else if (s[1].contains("мар")) {
-            s[1] = "03";
-        } else if (s[1].contains("фев")) {
-            s[1] = "02";
-        } else if (s[1].contains("янв")) {
-            s[1] = "01";
         }
 
-        String s1 = String.format(
-                "20%s-%s-%s %s:%s", s[2], s[1], s[0], s[3], s[4]);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-d HH:mm");
-        return LocalDateTime.parse(s1, formatter);
+        DateTimeFormatter fmt = new DateTimeFormatterBuilder()
+                .appendPattern("d ")
+                .appendText(ChronoField.MONTH_OF_YEAR, monthName)
+                .appendPattern(" yy, ")
+                .appendPattern("HH:mm")
+                .toFormatter();
+
+        return LocalDateTime.parse(parse56, fmt);
     }
 }
